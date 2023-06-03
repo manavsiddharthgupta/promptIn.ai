64d42051df279a5f6edc0ac1c792dceae7497cfe
devansh dwivedi <devanshdwivedi68@gmail.com>
2023-06-03 12:55:36 +0530

    feature: added signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,63 @@
+"use client"
+import Button from "../components/Button";
+import InputField from "../components/InputField";
+import useInputField from "../utils/hooks/useInputField";
+
 export default function SignUp() {
+  const {inputValue: avatarName, onChangeHandler: onAvatarNameChangeHandler} = 
+    useInputField((value) => {
+      const avatarRegex = /^[A-Za-z0-9]{5,}$/;
+      return avatarRegex.test(value);
+    })
+
+  const {inputValue: password, onChangeHandler: onPasswordChangeHandler}= 
+    useInputField((value) => {
+      const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+      return passwordRegex.test(value);
+    })
+
+  const {inputValue: confirmPassword, onChangeHandler: onConfirmPasswordChangeHandler}= 
+    useInputField((value) => {
+      const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+      return passwordRegex.test(value);
+    })
+  
   return (
     <main className="min-h-screen flex items-center justify-center">
       <section>
         <p>SignIn</p>
-        <form></form>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(avatarName, password, confirmPassword)
+          }}
+          >
+          <InputField
+            value={avatarName}
+            type="text"
+            placeholder="Enter your Avatar Name"
+            label="Avatar Name"
+            onChangeHandler={onAvatarNameChangeHandler}
+            />
+          
+          <InputField
+            value={password}
+            type="text"
+            placeholder="Enter your password"
+            label="Password"
+            onChangeHandler={onPasswordChangeHandler}
+            />
+
+          <InputField
+            value={confirmPassword}
+            type="text"
+            placeholder="Enter your password again"
+            label="Password"
+            onChangeHandler={onConfirmPasswordChangeHandler}
+            />
+
+            <Button type="submit">Sign Up</Button>
+        </form>
       </section>
     </main>
   );
