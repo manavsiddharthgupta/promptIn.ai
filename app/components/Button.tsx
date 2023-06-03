f6cff5eff514f354ae988edb91eac35cf03912b9
Manav Gupta <2006468@kiit.ac.in>
2023-06-03 18:43:18 +0530

    ui: added css in signIn component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,16 +1,24 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
 interface ButtonProps {
-  type: "button" | "submit" | "reset";
-  children: React.ReactNode;
+	type: 'button' | 'submit' | 'reset';
+	children: React.ReactNode;
+	className?: string;
 }
 
-const Button: FC<ButtonProps> = ({ children, type }) => {
-  return (
-    <button className="bg-blue-500 text-white rounded-sm py-1 px-3" type={type}>
-      {children}
-    </button>
-  );
+const Button: FC<ButtonProps> = ({ children, type, className }) => {
+	return (
+		<button
+			className={
+				className
+					? className
+					: 'bg-blue-500 text-white rounded-sm pt-1 px-3 pb-[5px]'
+			}
+			type={type}
+		>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
