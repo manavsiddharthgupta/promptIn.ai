c559edb90cd57e28be61932f0f99031be8ffc648
Manav Gupta <2006468@kiit.ac.in>
2023-06-06 23:17:06 +0530

    updated: change layout for create-prompt page

---
+++ app/create-prompt/page.tsx
@@ -0,0 +1,13 @@
+import Card from '../ui/Card';
+
+const CreatePrompt = () => {
+  return (
+    <main className="px-4 min-[400px]:px-8 min-[600px]:px-4 lg:px-0 mt-8">
+      <Card>
+        <p>Create page</p>
+      </Card>
+    </main>
+  );
+};
+
+export default CreatePrompt;
