c50325f2d94cc13032b0eeb16e0e3dc9fa10ba53
Manav Gupta <2006468@kiit.ac.in>
2023-06-19 15:40:55 +0530

    fix: bacj to home btn

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -7,18 +7,19 @@ import TitleInputField from '../ui/TitleInputField';
 import PromptTextArea from '../ui/PromptTextArea';
 import PromptTagsField from '../ui/PromptTagsField';
 import PromptHashTagField from '../ui/PromptHashTagField';
+import Link from 'next/link';
 
 const CreatePrompt = () => {
   return (
     <main className="px-4 min-[400px]:px-8 min-[600px]:px-4 lg:px-0 mt-8">
       <Card>
-        <Button
-          type="button"
-          className="text-sm px-4 bg-blue-500 text-white pb-[3.25px] pt-[3px] rounded-lg flex gap-2 items-center"
+        <Link
+          href="/"
+          className="text-sm px-4 bg-blue-500 text-white pb-[3.25px] pt-[3px] rounded-lg flex gap-2 items-center w-fit"
         >
           <FontAwesomeIcon icon={faArrowLeft} className="w-4 mr-1 pt-[0.5px]" />
           back to home
-        </Button>
+        </Link>
         <p className="font-bold text-4xl my-4">Create your prompt</p>
         <form
           onSubmit={(event) => {
