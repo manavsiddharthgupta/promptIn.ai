9a07c06a1d995cce3cf8f5711135a19a12614c86
Manav Gupta <2006468@kiit.ac.in>
2023-07-02 12:25:54 +0530

    feature: edit btn in profile page

--- app/ui/Icons.tsx
+++ app/ui/Icons.tsx
@@ -1,6 +1,8 @@
 'use client';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkIcon from '@mui/icons-material/Link';
+import EditIcon from '@mui/icons-material/Edit';
+import { IconButton, Tooltip } from '@mui/material';
 
 export const MailIcon = () => {
   return <EmailIcon fontSize="small" />;
@@ -8,4 +10,14 @@ export const MailIcon = () => {
 
 export const WebLink = () => {
   return <LinkIcon fontSize="small" />;
+};
+
+export const EditBtn = () => {
+  return (
+    <Tooltip title="Edit Profile">
+      <IconButton className="hover:bg-black bg-[#0000005a]">
+        <EditIcon className="text-white" fontSize="small" />
+      </IconButton>
+    </Tooltip>
+  );
 };
