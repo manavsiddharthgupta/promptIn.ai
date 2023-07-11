8221d2ff3fa13c4d1083a550931693397ddc30e3
Manav Gupta <2006468@kiit.ac.in>
2023-07-11 20:19:28 +0530

    fix: prompt tags ui

--- app/components/prompt card/PromptHastags.tsx
+++ app/components/prompt card/PromptHastags.tsx
@@ -10,13 +10,17 @@ export const PromptAllTags = ({
   const promptTags = tags.map((tag) => (
     <PormptTag key={tag.slug} tag={tag.name} />
   ));
-  return <div className="mt-[2px] px-1 font-normal w-full">{promptTags}</div>;
+  return (
+    <div className="mt-[2px] px-1 font-normal w-full flex flex-wrap">
+      {promptTags}
+    </div>
+  );
 };
 
 export const PormptTag = ({ tag }: { tag: string }) => {
   return (
-    <span className="inline-block mb-1 w-fit px-2 py-[1px] bg-[#0000001d] rounded-xl mr-1 text-xs">
+    <p className="mb-1 w-fit px-2 py-[1px] bg-[#0000001d] rounded-xl mr-1 text-xs">
       {tag}
-    </span>
+    </p>
   );
 };
